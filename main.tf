f745ff4fae7c2f5b2bd820787e7f73285f2a617f
Christopher Ozurumba <67044030+Mamiololo01@users.noreply.github.com>
2023-04-05 09:04:19 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.55.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "<Backend State S3 Bucket Name>"
+    key            = "terraform/terraform.tfstate"
+    dynamodb_table = "<Dependency Lock DynamoDB Table Name>"
+  }
+}
+
+resource "aws_s3_bucket" "s3" {
+  bucket = "<Terraform S3 Bucket Name>"
+}
